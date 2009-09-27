c7cf162aa26b109afaa656b50cc296003ee969b2
Ronny Brendel <ronnybrendel@gmail.com>
2009-09-27 16:43:10 +0200

    refinement of the filter build script
    - recognizes cpp/c extensions and compiles using gcc/g++ accordingly
    - changed commandline arguments $1 = target, $2 = target directory

--- src/filters/build.sh
+++ src/filters/build.sh
@@ -1,15 +1,35 @@
-TARGET_DIRECTORY=$1"./"
+#! /bin/bash
 
-SOURCE_DIRECTORY=`echo $0 | sed 's/\/[^\/]*$//g'`
-SOURCES=`ls ${SOURCE_DIRECTORY} | grep -e "\.cpp$" -e "\.c$"`
+SCRIPT_DIRECTORY=`echo $0 | sed 's/\/[^\/]*$//g'`
+SOURCES=`ls ${SCRIPT_DIRECTORY} | grep -e "\.cpp$" -e "\.c$"`
+
+TARGET=$1
+#compile only one file if a target has been specified
+if [ "$TARGET" != "" ]; then
+    SOURCES=${TARGET}
+fi
+
+TARGET_DIRECTORY=$2
+if [ "$TARGET_DIRECTORY" == "" ]; then
+    TARGET_DIRECTORY="."
+fi
 
 
 for SOURCE in ${SOURCES};
 do
+    EXTENSION=`echo "$SOURCE" | sed 's/.*\.cpp/cpp/g' | sed 's/.*\.c/c/g'`
     SOURCE_WITHOUT_EXTENSION=`echo "${SOURCE}" | sed -e 's/\.cpp//g' -e 's/\.c//g'`
     TARGET_WITH_PATH="${TARGET_DIRECTORY}/lib${SOURCE_WITHOUT_EXTENSION}.so"
-    SOURCE_WITH_PATH=${SOURCE_DIRECTORY}/${SOURCE}
-    echo "gcc -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}"
-    g++ -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}
+    SOURCE_WITH_PATH=${SCRIPT_DIRECTORY}/${SOURCE}
+
+    if [ "$EXTENSION" == "cpp" ]; then
+        echo "g++ -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}"
+        g++ -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}
+    elif [ "$EXTENSION" == "c" ]; then
+        echo "gcc -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}"
+        gcc -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}
+    else
+        echo "unrecognized extenstion $EXTENSION $SOURCE"
+    fi
 done
 
