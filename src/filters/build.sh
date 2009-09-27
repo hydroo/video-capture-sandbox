ada3de1b08a62f977771c8637ecde2473e490954
Ronny Brendel <ronnybrendel@gmail.com>
2009-09-27 17:46:28 +0200

    refactored build script
     - removed all { and }
     - removed explicit target specification, since it causes some trouble and is not really needed for now
     - general beautification

--- src/filters/build.sh
+++ src/filters/build.sh
@@ -1,41 +1,38 @@
 #! /bin/bash
 
-SCRIPT_DIRECTORY=$(echo $0 | sed 's/\/[^\/]*$//g')
-SOURCES=$(ls ${SCRIPT_DIRECTORY} | grep -e "\.cpp$" -e "\.c$")
+SCRIPT_DIRECTORY=$(echo "$0" | sed 's/\/[^\/]*$//g')
+SOURCES=$(ls $SCRIPT_DIRECTORY | grep -e "\.cpp$" -e "\.c$")
 
-TARGET=$1
-#compile only one file if a target has been specified
-if [ "$TARGET" != "" ]; then
-    SOURCES=${TARGET}
-fi
-
-TARGET_DIRECTORY=$2
+TARGET_DIRECTORY=$1
 if [ "$TARGET_DIRECTORY" == "" ]; then
     TARGET_DIRECTORY="."
 fi
 
 
-for SOURCE in ${SOURCES};
+for SOURCE in $SOURCES;
 do
-    EXTENSION=$(echo "$SOURCE" | sed 's/.*\.cpp/cpp/g' | sed 's/.*\.c/c/g')
     SOURCE_WITHOUT_EXTENSION=$(echo "$SOURCE" | sed -e 's/\.cpp//g' -e 's/\.c//g')
-    TARGET_WITH_PATH="${TARGET_DIRECTORY}/lib${SOURCE_WITHOUT_EXTENSION}.so"
-    SOURCE_WITH_PATH=${SCRIPT_DIRECTORY}/${SOURCE}
+    SOURCE_WITH_PATH=$SCRIPT_DIRECTORY/$SOURCE
+    TARGET_WITH_PATH="$TARGET_DIRECTORY/lib$SOURCE_WITHOUT_EXTENSION.so"
 
-    SOURCE_MODIFICATION_TIME=$(stat -c "%Y" $SOURCE_WITH_PATH)
-    TARGET_MODIFICATION_TIME=$(stat -c "%Y" $TARGET_WITH_PATH)
+
+    SOURCE_MODIFICATION_TIME="$(stat -c "%Y" $SOURCE_WITH_PATH)"
+    #trick: that 0 makes not finding the file work
+    TARGET_MODIFICATION_TIME="0$(stat -c "%Y" $TARGET_WITH_PATH 2>/dev/null)"
 
     if [ "$SOURCE_MODIFICATION_TIME" -ge "$TARGET_MODIFICATION_TIME" ]; then
 
-        if [ "$EXTENSION" == "cpp" ]; then
-            echo "g++ -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}"
-            g++ -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}
-        elif [ "$EXTENSION" == "c" ]; then
-            echo "gcc -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}"
-            gcc -shared -fPIC ${SOURCE_WITH_PATH} -o ${TARGET_WITH_PATH}
-        else
-            echo "unrecognized extenstion $EXTENSION $SOURCE"
+        EXTENSION=$(echo "$SOURCE" | sed 's/.*\.cpp/cpp/g' | sed 's/.*\.c/c/g')
+
+        if [ "$EXTENSION" == "cpp" ]; then CC="g++"
+        elif [ "$EXTENSION" == "c" ]; then CC="gcc"
+        else exit -1
         fi
+
+        COMMAND="$CC -shared -fPIC $SOURCE_WITH_PATH -o $TARGET_WITH_PATH"
+
+        echo "$COMMAND"
+        eval "$COMMAND"
 
     fi
 done
